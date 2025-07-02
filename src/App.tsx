590b5ff4297cc804e301532e8edfed46e3720205
Emirlan Dogdurbaev <jigit38@gmail.com>
2025-07-02 13:57:41 +0600

    delete imports

--- src/App.tsx
+++ src/App.tsx
@@ -40,19 +40,17 @@ function App() {
           <Route path="/dispatcher" element={<DispatcherDashboard />} />
           <Route path="/requests/:id" element={<RequestDetail />} />
           <Route path="/cars" element={<DriverCarAssignment />} />
-          <Route path="/test" element={<Test />} />
+
           <Route path="/report" element={<Reports />} />
         </Route>
 
         <Route element={<ProtectedRoute allowedRoles={["user"]} />}>
           <Route path="/user/dashboard" element={<Home />} />
           <Route path="/map" element={<MapPage />} />
-          <Route path="/test" element={<Test />} />
         </Route>
 
         <Route element={<ProtectedRoute allowedRoles={["driver"]} />}>
           <Route path="/driver/dashboard" element={<DriverDashboard />} />
-          <Route path="/test" element={<Test />} />
         </Route>
 
         <Route path="*" element={<Navigate to="/login" replace />} />
